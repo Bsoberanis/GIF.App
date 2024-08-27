3f6a0d73bcd92bb5f1fe9a2113ff07da979d7496
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2024-08-27 11:51:31 -0600

    Update useGif.jsx

--- src/hooks/useGif.jsx
+++ src/hooks/useGif.jsx
@@ -5,7 +5,7 @@ export const useGif = () => {
     const [gif, setGif] = useState('')
     
     const reqGif = async() => {
-        await fetch('https://api.giphy.com/v1/gifs/random?api_key=RcCR91TAv9j2mGomD9ZGoOnl8qZgm7vB').then(async(resp) => {
+        await fetch('https://api.giphy.com/v1/gifs/random?api_key=NVnyAr6E7rxBL0aSNmN0CCai12y5KLx8').then(async(resp) => {
             await resp.json().then(({data}) => {
                 setGif(data.images.original.url)
             })
