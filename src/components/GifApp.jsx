b7904b69bd9f24a183d991df4a82f7785eb25c13
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2024-08-27 11:41:41 -0600

    Update GifApp.jsx

--- src/components/GifApp.jsx
+++ src/components/GifApp.jsx
@@ -1,20 +1,5 @@
 export const GisfApp = () => {
 
-   const[gif , setGif] = useState('')
-
-    const url_image = 'https://www.azilen.com/blog/everything-must-know-spring-boot-application-scratch/'
-   
-    //async|await
-   
-    const reqGif =async() => {
-        await fetch('https://www.azilen.com/blog/everything-must-know-spring-boot-application-scratch/').then(async(resp)=>{
-            await resp.json().then((data)=> {
-                
-            })
-        } ).catch(console.error)
-
-    }
-
     return(
        <>
         <div>
@@ -24,8 +9,4 @@ export const GisfApp = () => {
 
     </> 
     )
-
-
-    
-    
-}
+    }
